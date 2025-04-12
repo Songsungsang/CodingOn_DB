013ee440186343930c7165226244f1960d3a0720
Songsungsang <mrcomsu@gmail.com>
2025-04-12 13:46:56 +0900

    [Chore] Small grammar change

--- Python_DB_connect/db.py
+++ Python_DB_connect/db.py
@@ -10,7 +10,9 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = (
+    f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 engine = create_engine(DATABASE_URL) # DB 접근할 엔진
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # 엔진 관리할 세션
